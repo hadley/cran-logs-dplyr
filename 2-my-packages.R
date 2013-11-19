06722ec92418886549a82b72f1e81d2849193432
hadley <h.wickham@gmail.com>
2013-11-18 19:02:15 -0600

    More exploration of my dev packages

--- 2-my-packages.R
+++ 2-my-packages.R
@@ -16,6 +16,13 @@ qplot(date, n, data = counts, geom = "line", colour = package)
 
 # So aggregate by week
 counts$week <- floor_date(counts$date, "week")
-by_week <- counts %.% group_by(week) %.% summarise(n = sum(n))
+by_week <- counts %.% group_by(week) %.% summarise(n = sum(n), days = n()) %.%
+  filter(days == 7)
 
 qplot(week, n, data = by_week, geom = "line", colour = package)
+
+dev_packages <- by_week %.% ungroup() %.% 
+  filter(package %in% c("devtools", "roxygen2", "testthat"))
+qplot(week, n, data = dev_packages, geom = "line", colour = package)
+# Some interesting difference here - devtools clearly most popular, and getting
+# relatively more popular over time.
