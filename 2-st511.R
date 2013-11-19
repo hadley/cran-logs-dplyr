f572ccfe8013e2c9a740c5895c437710151730bb
hadley <h.wickham@gmail.com>
2013-11-18 18:16:03 -0600

    Enhance stat511 detection

--- 2-st511.R
+++ 2-st511.R
@@ -1,5 +1,5 @@
 # Can we find Charlotte's ST511 students? They downloaded ggplot2, openintro,
-# Sleuth2, and knitr.
+# Sleuth2, evaluate and knitr: http://stat511.cwick.co.nz/labs/lab-2.html.
 library(dplyr)
 library(ggplot2)
 
@@ -9,7 +9,7 @@ recent <- filter(logs, date > as.Date("2013-09-25"))
 by_person <- group_by(recent, date, ip_id)
 
 # For each person, figure out how many of the matching packages they downloaded
-st511 <- c("ggplot2", "openintro", "Sleuth2", "knitr")
+st511 <- c("Sleuth3", "openintro", "evaluate", "ggplot2", "knitr")
 matches <- summarise(by_person, match = sum(unique(package) %in% st511))
 
 # Three approaches
@@ -19,11 +19,11 @@ matches <- summarise(by_person, match = sum(unique(package) %in% st511))
 
 daily_match <- matches %.% group_by(match) %.% summarise(n = n())
 
-# Downloads with 2+ matches swamped by 0 matches
+# Downloads with 2+ matches swamped by 0 matches, and also strong weekly pattern
 qplot(date, n, data = daily_match, colour = factor(match), geom = "line")
 
-# So convert to props, and remove 0 and 1 matches
-props <- daily_match %.% mutate(prop = n / sum(n)) %.% filter(match > 1, prop < 0.05)
+# So convert to props, and look at at least 3 matches
+props <- daily_match %.% mutate(prop = n / sum(n)) %.% filter(match > 2, prop < 0.1)
 qplot(date, n, data = props, colour = factor(match), geom = "line")
 qplot(date, prop, data = props, colour = factor(match), geom = "line")
 
