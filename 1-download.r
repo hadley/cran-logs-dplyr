156f4cd490155548c5a103a4ad6d69bef220dbc8
hadley <h.wickham@gmail.com>
2013-11-25 16:49:25 -0600

    More informative output

--- 1-download.r
+++ 1-download.r
@@ -1,6 +1,7 @@
 library(plyr)
 
 # Make sure we have all the logs -----------------------------------------------
+message("Downloading logs")
 
 start <- as.Date('2013-01-01')
 yesterday <- Sys.Date() - 1
@@ -16,15 +17,17 @@ ok <- Map(download.file, urls[missing], paths[missing])
 
 # Read all csv files and merge -------------------------------------------------
 
+message("Parsing logs")
 logs <- dir("logs", full.name = TRUE)
-all_pkgs <- lapply(logs, function(file){
+all_pkgs <- llply(logs, function(file){
   data <- read.csv(file, stringsAsFactors = FALSE)
   data$r_version <- as.character(data$r_version)
   data$r_arch    <- as.character(data$r_arch)
   data$r_os      <- as.character(data$r_os)
   data
-})
+}, .progress = "text")
 
+rbind_list(all_pkgs)
 all <- rbind.fill(all_pkgs)
 all$date <- as.Date(all$date)
 class(all) <- c("tbl_cpp", "tbl", class(all))
